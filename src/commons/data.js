8c2ce097919a31b5e1a4d42abdc1f74afbece6cf
marcusdevsurfer <marcus.sea13@gmail.com>
2024-08-25 17:17:45 -0600

    refact: addition of data commons

---
+++ src/commons/data.js
@@ -0,0 +1,61 @@
+export const units = [
+    {
+        carrier: {
+            name: 'Transportes Flores',
+            email: 'transportesflores@gmail.com',
+            phone: '3141986645'
+        },
+        unit: {
+            ton: '3.5',
+            service: ''
+        }
+    },
+    {
+        carrier: {
+            name: 'Transportes Manzanillo',
+            email: 'transportesmanzanillo@gmail.com',
+            phone: '3148765544'
+        },
+        unit: {
+            ton: '4.0',
+            service: ''
+        }
+    }
+    ,
+    {
+        carrier: {
+            name: 'Juan Ramos',
+            email: 'juanramos@gmail.com',
+            phone: '3148765544'
+        },
+        unit: {
+            ton: '5.0',
+            service: ''
+        }
+    }
+    ,
+    {
+        carrier: {
+            name: 'JASEZA',
+            email: 'jaseza@gmail.com',
+            phone: '3148765544'
+        },
+        unit: {
+            ton: '4.0',
+            service: ''
+        }
+    }
+    ,
+    {
+        carrier: {
+            name: 'Transportes Roman',
+            email: 'transportesroman@gmail.com',
+            phone: '3148765544'
+        },
+        unit: {
+            ton: '7.0',
+            service: ''
+        }
+    }
+
+]
