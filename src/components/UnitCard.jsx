f480295e83ab4e0026007a862a77b95ca6390fca
marcusdevsurfer <marcus.sea13@gmail.com>
2024-08-28 15:14:21 -0600

    refact: ui changes

--- src/components/UnitCard.jsx
+++ src/components/UnitCard.jsx
@@ -12,7 +12,7 @@ export const UnitCard = ({carrier, unit}) => {
                 <Card.Header>
                     <Stack direction='horizontal'>
                         <h5 className='m-0'>{carrier.name}</h5>
-                        <Button className='ms-auto' variant='primary' size='sm'>Ver perfil</Button>
+                        <Button style={{backgroundColor: '#3E8E41', borderColor: 'transparent'}} className='ms-auto' size='sm'>Ver perfil</Button>
                     </Stack>
                 </Card.Header>
 
