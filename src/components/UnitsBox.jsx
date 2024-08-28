c06b8231ecf20004db24a99c6fedf8ae7085cfec
marcusdevsurfer <marcus.sea13@gmail.com>
2024-08-28 12:23:50 -0600

    refact: code refact

--- src/components/UnitsBox.jsx
+++ src/components/UnitsBox.jsx
@@ -3,15 +3,18 @@ import { useState } from 'react'
 import { FindBar } from './FindBar'
 import { UnitList } from './UnitList'
 import { CarrierProfile } from './CarrierProfile'
+// Azul oscuro (#2F4F7F)
+// Gris claro (#F7F7F7)
+// Verde oscuro (#3E8E41)
+// Blanco (#FFFFFF)
 
 export const UnitsBox = () => {
     const [query, setQuery] = useState('')
     return (
-        <Container style={{ minHeight: '100vh' }} fluid className='bg-body-tertiary'>
+        <Container style={{ minHeight: '100vh', backgroundColor: '#2F4F7F' }} fluid>
             <Container >
-                <FindBar query={query} setQuery={setQuery} />
-                <CarrierProfile/>
-                {/* <UnitList query={query} /> */}
+                <FindBar query={query} setQuery={setQuery} /> 
+                <UnitList query={query} />
             </Container>
         </Container>
     )
