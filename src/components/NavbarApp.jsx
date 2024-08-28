f97f38bc61f6a8206d15f5d9ff1b57fb3562235e
marcusdevsurfer <marcus.sea13@gmail.com>
2024-08-28 12:23:09 -0600

    add: colors comments

--- src/components/NavbarApp.jsx
+++ src/components/NavbarApp.jsx
@@ -1,9 +1,14 @@
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 
+// Azul oscuro (#2F4F7F)
+// Gris claro (#F7F7F7)
+// Verde oscuro (#3E8E41)
+// Blanco (#FFFFFF)
+
 export const NavbarApp = () => {
     return (
-        <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+        <Navbar collapseOnSelect expand="lg" style={{ backgroundColor: '#F7F7F7' }} >
             <Container>
                 <Navbar.Brand href="#">Social</Navbar.Brand>
                 <Navbar.Toggle />
