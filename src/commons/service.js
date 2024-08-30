0aa8e2059903fa2d06ca145eef1a111f75e69654
marcusdevsurfer <marcus.sea13@gmail.com>
2024-08-30 16:05:01 -0600

    coed refactor

--- src/commons/service.js
+++ src/commons/service.js
@@ -1,5 +1,10 @@
-import { units } from "./data"
+import {  carriers } from "./data"
 
-export const findCarrierById = (id) => {
-    return units.find((e) => e.carrier.id == id)
-}
+// export const findCarrierById = (id) => {
+//     return units.find((e) => e.carrier.id == id)
+// }
+
+
+export const findCarrierById = (carrierId) => {
+    return carriers.find((e) => e.id == carrierId)
+} 
