7d21eb15dc4a7363d3f8f9bb79d626ac59987fb2
marcusdevsurfer <marcus.sea13@gmail.com>
2024-08-31 16:40:00 -0600

    add: addition of bg img

--- src/components/CarrierCover.jsx
+++ src/components/CarrierCover.jsx
@@ -1,16 +1,12 @@
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
-
 import './carrierCover.css'
-
 export const CarrierCover = ({ carrierName, carrierLogo }) => {
-
-
     return (
         <Row className='align-items-center text-center example'>
             <Col lg='7'>
-                <h1>{carrierName}</h1>
+                <h1 className='text-white bg-dark rounded p-3'>{carrierName}</h1>
             </Col>
             <Col>
                 <Image src={carrierLogo} width={400} roundedCircle fluid />
