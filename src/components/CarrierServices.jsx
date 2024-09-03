087ed3ce89a475477315f80ef89cfad1fd7be574
marcusdevsurfer <marcus.sea13@gmail.com>
2024-09-03 00:25:51 -0600

    code refactor and ui update

--- src/components/CarrierServices.jsx
+++ src/components/CarrierServices.jsx
@@ -1,24 +1,27 @@
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-
+import Card from 'react-bootstrap/Card'
+import { HiOutlineTruck, HiTruck } from 'react-icons/hi'
 export const CarrierServices = ({ services, isLoading }) => {
-
-    const serviceListItem = {
-        listStyle: 'none'
-    }
-
     return (
-        <Row style={{ minHeight: '45vh' }} className='align-items-center text-center'>
+        <Row style={{ minHeight: '50vh' }} >
             <Col>
-                <h1>Servicios</h1>
-                <ul>
-                    {
-                        !isLoading &&
+                <h2 className='text-bold text-center mt-5'>Nuestros Servicios</h2>
+                <Row className='mt-4'>
+                    {!isLoading &&
                         services.map((e) =>
-                            <li style={serviceListItem}>{e}</li>
+                            <Col className='mb-1'>
+                                <Card className='p-1'>
+                                    <Card.Body>
+                                        <Card.Text className='text-center'><HiOutlineTruck size={30} /></Card.Text>
+                                        <Card.Title className='mb-3 text-center'>{e}</Card.Title>
+                                        <Card.Text className='text-muted'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore corporis voluptates illo fugit, minima quod cum rem atque adipisci saepe, aliquid quis, inventore reiciendis accusantium iste dignissimos perspiciatis reprehenderit? Asperiores?</Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
                         )
                     }
-                </ul>
+                </Row>
             </Col>
         </Row>
     )
