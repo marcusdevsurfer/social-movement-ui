18dcf51fff174bf38539eda124fb623352f6a86e
marcusdevsurfer <marcus.sea13@gmail.com>
2024-09-03 17:31:19 -0600

    ui changes

--- src/components/CarrierCover.jsx
+++ src/components/CarrierCover.jsx
@@ -4,11 +4,17 @@ import Image from 'react-bootstrap/Image'
 import './carrierCover.css'
 
 export const CarrierCover = ({ carrierName, carrierLogo, carrierSlogan }) => {
+
+    //Titulos
+    const titleColor = '#020817'
+    //Letra de subtitulo
+    const subtitle = '#5E6D82'
+
     return (
         <Row className='align-items-center hero'>
             <Col sm="12" md="6" lg="6" className='p-5'>
-                <h1 className='text-start'>{carrierName}</h1>
-                <p className='text-start' style={{textAlign: 'justify'}}>{carrierSlogan}</p>
+                <h1 style={{ color: titleColor }} className='text-start'>{carrierName}</h1>
+                <p className='text-start' style={{ textAlign: 'justify', color: subtitle }}>{carrierSlogan}</p>
             </Col>
             <Col className='text-center'>
                 <Image src={carrierLogo} width={350} roundedCircle fluid />
--- src/components/NavbarApp.jsx
+++ src/components/NavbarApp.jsx
@@ -1,22 +1,31 @@
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 
-// Azul oscuro (#2F4F7F)
-// Gris claro (#F7F7F7)
-// Verde oscuro (#3E8E41)
-// Blanco (#FFFFFF)
+
+//Colores
+
+
+//Letra de subtitulo
+const subtitle = '#5E6D82'
+//Navbar background color
+const navbarBg = '#2563EB'
+//Background segunda seccion
+const bgSectionTwo = '#F1F5F9'
+//Titulos
+const titleColor = '#020817'
+
 
 export const NavbarApp = () => {
     return (
-        <Navbar collapseOnSelect expand="lg" style={{ backgroundColor: 'rgb(10, 133, 55)', minHeight: '8vh' }} >
+        <Navbar collapseOnSelect expand="lg" style={{ backgroundColor: navbarBg, minHeight: '8vh' }} >
             <Container>
                 <Navbar.Brand href="/" className="text-white">Social</Navbar.Brand>
-                <Navbar.Toggle className="text-white" />
+                {/* <Navbar.Toggle className="text-white" />
                 <Navbar.Collapse className="justify-content-end">
                     <Navbar.Text className="text-white">
                         Sesión iniciada como: <a className="text-white" href="#">Mark Otto</a>
                     </Navbar.Text>
-                </Navbar.Collapse>
+                </Navbar.Collapse> */}
             </Container>
         </Navbar>
     )
