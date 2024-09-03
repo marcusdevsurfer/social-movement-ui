94b2b966db44aaff9bd80be1945ea9f71e9ae31c
marcusdevsurfer <marcus.sea13@gmail.com>
2024-09-03 17:01:23 -0600

    fix grid in services component

--- src/components/CarrierServices.jsx
+++ src/components/CarrierServices.jsx
@@ -4,10 +4,10 @@ import Card from 'react-bootstrap/Card'
 import { HiOutlineTruck, HiTruck } from 'react-icons/hi'
 export const CarrierServices = ({ services, isLoading }) => {
     return (
-        <Row style={{ minHeight: '50vh', boxShadow: "0rem 0.2rem 5rem 0.1rem gray"}} >
+        <Row className='' style={{ minHeight: '50vh', boxShadow: "0rem 0.2rem 5rem 0.1rem gray" }} >
             <Col>
                 <h2 className='text-center mt-5'>Nuestros Servicios</h2>
-                <Row className='mt-4'>
+                <Row className='mt-4 justify-content-center'>
                     {!isLoading &&
                         services.map((e) =>
                             <Col key={e} xs="12" md="6" lg="3" className='my-1'>
@@ -15,7 +15,8 @@ export const CarrierServices = ({ services, isLoading }) => {
                                     <Card.Body>
                                         <Card.Text className='text-center'><HiOutlineTruck size={30} /></Card.Text>
                                         <Card.Title className='mb-3 text-center'>{e}</Card.Title>
-                                        <Card.Text className='text-muted'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore corporis voluptates illo fugit, minima quod cum rem atque adipisci saepe, aliquid quis, inventore reiciendis accusantium iste dignissimos perspiciatis reprehenderit? Asperiores?</Card.Text>
+                                        <Card.Text className='text-muted'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam dignissimos nam ratione.
+                                        </Card.Text>
                                     </Card.Body>
                                 </Card>
                             </Col>
