332334bee4e186896a5943dcd9736135698a8fa6
marcusdevsurfer <marcus.sea13@gmail.com>
2024-09-03 17:14:05 -0600

    refact: ui changes

--- src/components/UnitsBox.jsx
+++ src/components/UnitsBox.jsx
@@ -2,18 +2,15 @@ import Container from 'react-bootstrap/Container'
 import { useState } from 'react'
 import { FindBar } from './FindBar'
 import { UnitList } from './UnitList'
-import { CarrierProfile } from './CarrierProfile'
-// Azul oscuro (#2F4F7F)
-// Gris claro (#F7F7F7)
-// Verde oscuro (#3E8E41)
-// Blanco (#FFFFFF)
 
 export const UnitsBox = () => {
     const [query, setQuery] = useState('')
     return (
-        <Container style={{ minHeight: '95vh', backgroundColor: '#2F4F7F' }} fluid>
-            <Container >
-                <FindBar query={query} setQuery={setQuery} /> 
+        <Container style={{ minHeight: '95vh', backgroundColor: '#F7F7F7' }} fluid>
+            <Container>
+                <FindBar query={query} setQuery={setQuery} />
+            </Container>
+            <Container>
                 <UnitList query={query} />
             </Container>
         </Container>
