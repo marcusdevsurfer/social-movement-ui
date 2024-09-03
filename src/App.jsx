a728d70f86912cc46070d82747232ad30befda2a
marcusdevsurfer <marcus.sea13@gmail.com>
2024-09-03 14:27:18 -0600

    fix: routing fixed and code clean

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,17 @@ import './App.css'
 import { UnitsBox } from "./components/UnitsBox"
 import { NavbarApp } from "./components/NavbarApp"
 import { CarrierProfile } from "./components/CarrierProfile"
-import { Route, Router, Switch } from "wouter"
-import { UnitCard } from './components/UnitCard'
+import { Route, Switch } from "wouter"
 
 function App() {
   return (
     <>
       <NavbarApp />
-      {/* Unidades compomente
-      <Route path={'/'} component={UnitsBox} />
-
-      {/* Perfil de la empresa 
-      <Router path="/profile" >
-        <Route path="/:id" component={CarrierProfile} >
-        </Route>
-      </Router> */}
-
       <Switch>
         <Route path="/" component={UnitsBox} />
         <Route path="/profile/:id" component={CarrierProfile} />
-        {/* 
-     in wouter, any Route with empty path is considered always active. 
-     This can be used to achieve "default" route behaviour within Switch. 
-     Note: the order matters! See examples below.
-  */}
         <Route>This is rendered when nothing above has matched</Route>
-      </Switch>;
+      </Switch>
     </>
   )
 }
