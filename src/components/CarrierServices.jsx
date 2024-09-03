6319d9cbd8f52f49f4cdcb7894148bd4aecf4498
marcusdevsurfer <marcus.sea13@gmail.com>
2024-09-03 15:05:22 -0600

    ui changes and grid addition

--- src/components/CarrierServices.jsx
+++ src/components/CarrierServices.jsx
@@ -4,13 +4,13 @@ import Card from 'react-bootstrap/Card'
 import { HiOutlineTruck, HiTruck } from 'react-icons/hi'
 export const CarrierServices = ({ services, isLoading }) => {
     return (
-        <Row style={{ minHeight: '50vh' }} >
+        <Row style={{ minHeight: '50vh', boxShadow: "0rem 0.2rem 5rem 0.1rem gray"}} >
             <Col>
                 <h2 className='text-center mt-5'>Nuestros Servicios</h2>
                 <Row className='mt-4'>
                     {!isLoading &&
                         services.map((e) =>
-                            <Col className='mb-1'>
+                            <Col key={e} xs="12" md="6" lg="3" className='my-1'>
                                 <Card className='p-1'>
                                     <Card.Body>
                                         <Card.Text className='text-center'><HiOutlineTruck size={30} /></Card.Text>
