3ae7862fa0412ed8b22887f3e436cac82ae696c9
marcusdevsurfer <marcus.sea13@gmail.com>
2024-09-12 14:11:41 -0600

    refact: ui refact and addition of img

--- src/components/CarrierCover.jsx
+++ src/components/CarrierCover.jsx
@@ -2,29 +2,15 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
 import './carrierCover.css'
-import { useEffect } from 'react'
-
 export const CarrierCover = ({ carrierName, carrierLogo, carrierSlogan }) => {
-
-    // useEffect(() => {
-    //     setInterval(() => console.log("Interval funcionando..."), 2500)
-
-    //     return clearInterval()
-    // }, [])
-
-    //Titulos
-    const titleColor = '#020817'
-    //Letra de subtitulo
-    const subtitle = '#5E6D82'
-
     return (
-        <Row className='align-items-center hero'>
-            <Col sm="12" md="6" lg="6" className='p-5'>
-                <h1 style={{ color: titleColor }} className='text-start'>{carrierName}</h1>
-                <p className='text-start' style={{ textAlign: 'justify', color: subtitle }}>{carrierSlogan}</p>
+        <Row className='align-items-center justify-content-center hero'>
+            <Col sm="12" md="6" lg="6" className='p-3'>
+                <h1 className='transport-name'>{carrierName}</h1>
+                <p className='transport-slogan'>{carrierSlogan}</p>
             </Col>
-            <Col className='text-center'>
-                <Image src={carrierLogo}  width={350} roundedCircle fluid />
+            <Col className='text-center p-3'>
+                <Image src={carrierLogo} width={350} roundedCircle fluid />
             </Col>
         </Row>
     )
