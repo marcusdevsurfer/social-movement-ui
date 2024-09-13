06425a21255d9e16d8df7b8723cb46ea0ee44ba7
marcusdevsurfer <marcus.sea13@gmail.com>
2024-09-12 21:16:31 -0600

    refact: ui refact

--- src/components/CarrierCover.jsx
+++ src/components/CarrierCover.jsx
@@ -1,16 +1,14 @@
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Image from 'react-bootstrap/Image'
+import Button from 'react-bootstrap/Button'
 import './carrierCover.css'
-export const CarrierCover = ({ carrierName, carrierLogo, carrierSlogan }) => {
+export const CarrierCover = ({ carrierName, carrierSlogan }) => {
     return (
-        <Row className='align-items-center justify-content-center hero'>
-            <Col sm="12" md="6" lg="6" className='p-3'>
+        <Row className='align-items-center hero'>
+            <Col>
                 <h1 className='transport-name'>{carrierName}</h1>
                 <p className='transport-slogan'>{carrierSlogan}</p>
-            </Col>
-            <Col className='text-center p-3'>
-                <Image src={carrierLogo} width={350} roundedCircle fluid />
+                <Button size='lg' variant='dark'><a href="#services"></a>Servicios</Button>
             </Col>
         </Row>
     )
