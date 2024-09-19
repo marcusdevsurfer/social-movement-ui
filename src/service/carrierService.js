1f92feb7164facc41517eace00cf619866bdd314
marcusdevsurfer <marcus.sea13@gmail.com>
2024-09-19 13:08:07 -0600

    add: carrier service

---
+++ src/service/carrierService.js
@@ -0,0 +1,7 @@
+import { units, carriers } from "../commons/data"
+
+export const getAllCarriers = () => carriers
+
+export const getCarrierById = (id) => {
+    return carriers.find((carrier) => carrier.id === id)
+}
